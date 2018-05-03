03406e0501c0eeb70f2dc9ca05e28851c81ebe13
zhuzf <zzf@xlbzone.com>
2018-05-03 14:55:53 +0800

    完成pwa的demo

---
+++ sw.js
@@ -0,0 +1,43 @@
+var cacheStorageKey = 'minimal-pwa-5'
+
+var cacheList = [
+  '/',
+  "index.html",
+  "main.css",
+  "e.jpg"
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheStorageKey)
+    .then(cache => cache.addAll(cacheList))
+    .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    Promise.all(
+      caches.keys().then(cacheNames => {
+        return cacheNames.map(name => {
+          if (name !== cacheStorageKey) {
+            return caches.delete(name)
+          }
+        })
+      })
+    ).then(() => {
+      return self.clients.claim()
+    })
+  )
+})
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      if (response != null) {
+        return response
+      }
+      return fetch(e.request.url)
+    })
+  )
+})
